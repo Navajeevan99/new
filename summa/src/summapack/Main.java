e6510da42862e56ca9015a847210522c7adb3fd9
Navajeevan99 <navajeevanks.23it@kongu.edu>
2024-09-13 18:20:41 +0530

    one

---
+++ summa/src/summapack/Main.java
@@ -0,0 +1,21 @@
+package summapack;
+class n
+{
+	void run()
+	{
+		
+	}
+}
+
+public class Main {
+	void raun()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
